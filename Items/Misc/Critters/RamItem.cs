e9b7bb99f1d73605e584211d0463b062df1c4ba4
Ebonfly <73665110+ebonfly@users.noreply.github.com>
2026-01-04 18:16:04 +0100

    feat: ram

---
+++ Items/Misc/Critters/RamItem.cs
@@ -0,0 +1,28 @@
+﻿using EbonianMod.NPCs.Overworld.Critters;
+
+namespace EbonianMod.Items.Misc.Critters;
+
+public class RamItem : ModItem
+{
+    public override void SetDefaults()
+    {
+        Item.CloneDefaults(ItemID.Bunny);
+        Item.makeNPC = NPCType<Ram>();
+        Item.value = Item.buyPrice(0, 0, 10, 0);
+    }
+}
+public class RamItemNaked : ModItem
+{
+    public override void SetDefaults()
+    {
+        Item.CloneDefaults(ItemID.Bunny);
+        Item.makeNPC = 0;
+        Item.value = Item.buyPrice(0, 0, 10, 0);
+    }
+    public override bool? UseItem(Player player)
+    {
+        if (player.whoAmI == Main.myPlayer)
+            MPUtils.NewNPC(Main.MouseWorld, NPCType<Ram>(), ai2: 1);
+        return base.UseItem(player);
+    }
+}
