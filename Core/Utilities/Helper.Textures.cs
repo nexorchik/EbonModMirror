944fcb6a179589b8137f9364e5b1b576850d38be
Ebonfly <73665110+ebonfly@users.noreply.github.com>
2026-01-28 09:44:17 +0100

    misc

--- Core/Utilities/Helper.Textures.cs
+++ Core/Utilities/Helper.Textures.cs
@@ -2,10 +2,10 @@ namespace EbonianMod.Core.Utilities;
 
 public static partial class Helper
 {
-	public static string AssetPath = "EbonianMod/Assets/Images/";
-	public static string BuffPlaceholder = Helper.AssetPath + "Buffs/ExolStun";
-	public static string Empty = Helper.AssetPath + "Extras/Empty";
-	public static string Placeholder = Helper.AssetPath + "Extras/Placeholder";
+	public const string AssetPath = "EbonianMod/Assets/Images/";
+	public const string BuffPlaceholder = AssetPath + "Buffs/ExolStun";
+	public const string Empty = AssetPath + "Extras/Empty";
+	public const string Placeholder = AssetPath + "Extras/Placeholder";
 	public static Asset<Texture2D> GetTexture(string path, AssetRequestMode assetRequestMode = AssetRequestMode.AsyncLoad)
 	{
 		return path.StartsWith("EbonianMod/") ? Request<Texture2D>(path, assetRequestMode) : Request<Texture2D>("EbonianMod/" + path, assetRequestMode);
